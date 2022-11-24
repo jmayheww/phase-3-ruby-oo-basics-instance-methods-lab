92cdf7c1ee299717f1c306c055db7b6ded94a91e
Joshua <joshsmayhew@gmail.com>
2022-11-24 12:54:13 +0700

    commit tree 71e371d3b8527122efea02d275fc79dbe6021557

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+# make dog class
+class Dog
+  # Class body
+  def bark
+    puts 'Woof!'
+  end
+
+  def sit
+    puts 'The Dog is sitting'
+  end
+end
+
+fido = Dog.new
+
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def talk
+    puts 'Hello World!'
+  end
+
+  def walk
+    puts 'The Person is walking'
+  end
+end
+
+josh = Person.new
+
+josh.talk
+josh.walk
